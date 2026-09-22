409f6e20da2379b72e823ec730951c6e495aa3f4
phoenix <phoenixzero4@gmail.com>
2026-09-22 08:26:39 -0400

    Added headers to files

---
+++ src/leetcode/medium/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package leetcode.medium;
+
+/**
+ * Phoenix
+ * Sep 22 2026
+ */
+
+/**
+ *  PROBLEM DESCRIPTION
+ *
+ */
+
+public class ContainerWithMostWater {
+
+  public static int maxArea(int[] height){
+    int mostWater = 0;
+
+    return mostWater;
+  }
+}
